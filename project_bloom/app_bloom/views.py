da2df200d417ec0d9f7407053b252e2b6a372c23
Eduardo Alves Lins <140346484+eduardoalins@users.noreply.github.com>
2023-10-16 17:32:45 -0300

    Update views.py

--- project_bloom/app_bloom/views.py
+++ project_bloom/app_bloom/views.py
@@ -6,3 +6,18 @@ def home(request):
     return HttpResponse("funcionando"),
 def hello(request):
     return HttpResponse("foi hello")
+    
+def gestao (request):
+    return render(request, 'gestao.html')
+
+def editargestao (request):
+    return render(request, 'editargestao.html')
+
+def doar (request):
+    return render(request, 'doar.html')
+
+def doarrecorrente (request):
+    return render(request, 'doarrecorrente.html')
+
+def doarunica (request):
+    return render(request, 'doarunica.html')
