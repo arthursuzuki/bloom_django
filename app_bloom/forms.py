f294ad75a06dbf2b012d23762bce7e78cf3eac3d
Arthur Suzuki <126833330+arthursuzuki@users.noreply.github.com>
2023-11-28 11:29:40 -0300

    Update forms.py

--- app_bloom/forms.py
+++ app_bloom/forms.py
@@ -1,13 +1,8 @@
 from django import forms
-from app_bloom.models import *
+from app_bloom.models import Crianca
 
 
 class CriancaForm(forms.ModelForm):
     class Meta:
         model = Crianca
-        fields = '__all__'
-
-class FeedbackPadrinhoForm(forms.ModelForm):
-    class Meta:
-        model = FeedbackPadrinho
         fields = '__all__'
